737bb9b2c27d9c5272cf2bedd3756da2613d3d7c
Gaurav Shrestha <gauravshresthh@gmail.com>
2021-11-12 14:20:51 +0545

    minor fixes

--- forge.config.js
+++ forge.config.js
@@ -1,12 +1,41 @@
 // If you have set config.forge to a JavaScript file path in package.json:
 // Only showing the relevant configuration for brevity
+
+const path = require('path');
+
 module.exports = {
+	packagerConfig: {
+		icon: path.join(__dirname, 'anydone-app-icon-desktop.png'),
+	},
 	makers: [
 		{
-			name: '@electron-forge/maker-zip',
-			platforms: ['darwin', 'linux'],
+			name: '@electron-forge/maker-squirrel',
 			config: {
-				// Config here
+				name: 'Anydone inbox desk',
+				authors: 'treeleaf anydone team',
+				icon: path.join(__dirname, 'anydone-app-icon-desktop.png'),
+			},
+		},
+		{
+			name: '@electron-forge/maker-zip',
+			platforms: ['darwin'],
+			config: {
+				name: 'Anydone inbox desk',
+				icon: path.join(__dirname, 'anydone-app-icon-desktop.png'),
+			},
+		},
+		{
+			name: '@electron-forge/maker-deb',
+			config: {
+				name: 'Anydone inbox desk',
+				icon: path.join(__dirname, 'anydone-app-icon-desktop.png'),
+			},
+		},
+		{
+			name: '@electron-forge/maker-rpm',
+			config: {
+				name: 'Anydone inbox desk',
+				icon: path.join(__dirname, 'anydone-app-icon-desktop.png'),
 			},
 		},
 	],
